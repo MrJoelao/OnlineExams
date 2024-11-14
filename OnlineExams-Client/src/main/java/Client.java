3d3ca79865fd0cf1d220b4995c29c48621e94059
joelao <joel.stephan.email@gmail.com>
2024-11-14 20:28:18 +0100

    Adesso il client se non individua in server resta in attesa

--- OnlineExams-Client/src/main/java/Client.java
+++ OnlineExams-Client/src/main/java/Client.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
@@ -7,48 +6,47 @@ public class Client {
         String host = "localhost";
         int port = 12345;
 
+        ClientConnection clientConnection = new ClientConnection(host, port);
+
         try {
-            ClientConnection clientConnection = new ClientConnection(host, port);
-            System.out.println("Connected to server at " + host + ":" + port);
+            clientConnection.autoConnect();
 
-            try (ObjectOutputStream out = new ObjectOutputStream(clientConnection.getSocket().getOutputStream());
-                 ObjectInputStream in = new ObjectInputStream(clientConnection.getSocket().getInputStream())) {
+            Scanner scanner = new Scanner(System.in);
 
-                Scanner scanner = new Scanner(System.in);
+            while (true) {
+                // Riceve oggetto dal server
+                Object receivedObject = clientConnection.receive();
 
-                while (true) {
-                    Object receivedObject = in.readObject();
-
-                    if (receivedObject instanceof Signal) {
-                        Signal signal = (Signal) receivedObject;
-                        if (Signal.END.equals(signal.getType())) {
-                            System.out.println("End of communication.");
-                            break;
-                        } else if (Signal.START.equals(signal.getType())) {
-                            System.out.println("Start of questions.");
-                            continue;
-                        }
-                    }
-
-                    if (receivedObject instanceof Question) {
-                        Question question = (Question) receivedObject;
-                        System.out.println("Received question: " + question.getText());
-                        System.out.println("Options: " + question.getOptions());
-
-                        System.out.print("Your answer: ");
-                        String answer = scanner.nextLine();
-
-                        out.writeObject(answer);
-                        out.flush();
+                if (receivedObject instanceof Signal) {
+                    Signal signal = (Signal) receivedObject;
+                    if (Signal.END.equals(signal.getType())) {
+                        System.out.println("End of communication.");
+                        break;
+                    } else if (Signal.START.equals(signal.getType())) {
+                        System.out.println("Start of questions.");
+                        continue;
                     }
                 }
-            } catch (ClassNotFoundException e) {
+
+                if (receivedObject instanceof Question) {
+                    Question question = (Question) receivedObject;
+                    System.out.println("Received question: " + question.getText());
+                    System.out.println("Options: " + question.getOptions());
+
+                    System.out.print("Your answer: ");
+                    String answer = scanner.nextLine();
+
+                    clientConnection.send(answer);
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientConnection.close();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            clientConnection.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
